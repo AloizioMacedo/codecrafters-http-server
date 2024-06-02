7cad0b6db44e1bcf28efd1fb40e59d055d7bbe66
AloizioMacedo <atsam2@hotmail.com>
2024-06-02 15:12:25 -0300

    Extract url path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 // Uncomment this block to pass the first stage
 use anyhow::Result;
-use std::{io::Write, net::TcpListener};
+use itertools::Itertools;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,8 +16,25 @@ fn main() -> Result<()> {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write_all(ok().as_bytes())?;
-                println!("{}", ok());
+                let mut buf = [0; 1024];
+                _ = stream.read(&mut buf)?;
+
+                let contents = String::from_utf8_lossy(&buf);
+                let (_, path) = extract_full_url(&contents);
+
+                if path == "/" {
+                    stream.write_all(ok().as_bytes()).map_err(|e| {
+                        eprintln!("{e}");
+                        e
+                    })?;
+                } else {
+                    stream.write_all(not_found().as_bytes()).map_err(|e| {
+                        eprintln!("{e}");
+                        e
+                    })?;
+                }
+
+                stream.flush()?;
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -26,4 +47,27 @@ fn main() -> Result<()> {
 
 fn ok() -> String {
     String::from("HTTP/1.1 200 OK\r\n\r\n")
+}
+
+fn not_found() -> String {
+    String::from("HTTP/1.1 404 Not Found\r\n\r\n")
+}
+
+fn extract_full_url(request: &str) -> (&str, &str) {
+    let (request_line, headers) = request
+        .split_once("\r\n")
+        .expect("request is not well formed.");
+
+    let (_, path, _) = request_line
+        .splitn(3, ' ')
+        .collect_tuple()
+        .expect("request is ill-formed");
+
+    let (host, _) = headers
+        .split_once("\r\n")
+        .expect("request is not well formed");
+
+    let host = &host[6..];
+
+    (host, path)
 }
