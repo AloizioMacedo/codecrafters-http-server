642fce78aff4b4dd69ee446277dcebffde6c207f
AloizioMacedo <atsam2@hotmail.com>
2024-06-02 14:33:22 -0300

    Solve responding with 200

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use anyhow::Result;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -9,12 +10,20 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all(ok().as_bytes())?;
+                println!("{}", ok());
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn ok() -> String {
+    String::from("HTTP/1.1 200 OK\r\n\r\n")
 }
